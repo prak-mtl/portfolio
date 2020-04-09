d159bc9f2e5851b8df61b1bf1642b657623dfa81
Prakhar <34737618+prakhargloify@users.noreply.github.com>
2020-04-09 20:46:00 +0530

    Updates

---
+++ precache-manifest.969669257ba313eaaa6c04c669d3e95d.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e99e7700fc7cd53f877e853125ace2de",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "1db7d0ec3b82fea4c8f6",
+    "url": "/portfolio/static/css/main.61a50514.chunk.css"
+  },
+  {
+    "revision": "a3322d7d736bfcf4d69a",
+    "url": "/portfolio/static/js/2.16d9ee9a.chunk.js"
+  },
+  {
+    "revision": "1db7d0ec3b82fea4c8f6",
+    "url": "/portfolio/static/js/main.29a081db.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "ca1c2008d6de067696597fa41ba836b4",
+    "url": "/portfolio/static/media/address-book.ca1c2008.png"
+  },
+  {
+    "revision": "52d8bbdd76d512f2ec27c75718aa19e6",
+    "url": "/portfolio/static/media/currency.52d8bbdd.png"
+  },
+  {
+    "revision": "78bc3797d46fe3000c2f674f975cfab4",
+    "url": "/portfolio/static/media/magic-mirror.78bc3797.jpg"
+  },
+  {
+    "revision": "810e5d8eb1be599bd8c772ad512b81ed",
+    "url": "/portfolio/static/media/profile.810e5d8e.png"
+  },
+  {
+    "revision": "7ccb9941a2d63ca607ec6b1d172627c5",
+    "url": "/portfolio/static/media/weather.7ccb9941.png"
+  }
+]);
